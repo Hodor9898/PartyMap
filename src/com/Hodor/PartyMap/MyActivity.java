bce482ee06e94d976a19332d07a9876336c95918
Matan <mbytester9898@gmail.com>
2014-06-07 13:56:12 +0300

    initalize project

---
+++ src/com/Hodor/PartyMap/MyActivity.java
@@ -0,0 +1,16 @@
+package com.Hodor.PartyMap;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.example.PartyMap.R;
+
+public class MyActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
